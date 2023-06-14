2c670a612865f37d526eb62ca146438361527d6c
jeronimoalbi <jeronimo.albi@gmail.com>
2023-06-14 17:36:30 +0200

    refactor: update plugin to use gRPC

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/gob"
+	"context"
 	"os"
 
 	hplugin "github.com/hashicorp/go-plugin"
@@ -11,51 +11,34 @@ import (
 	"github.com/ignite/cli-plugin-network/cmd"
 )
 
-func init() {
-	gob.Register(plugin.Manifest{})
-	gob.Register(plugin.ExecutedCommand{})
-	gob.Register(plugin.ExecutedHook{})
+type p struct {
+	plugin.Interface
 }
 
-type p struct{}
-
-func (p) Manifest() (plugin.Manifest, error) {
-	m := plugin.Manifest{
+func (p) Manifest(_ context.Context) (*plugin.Manifest, error) {
+	m := &plugin.Manifest{
 		Name: "network",
 	}
 	m.ImportCobraCommand(cmd.NewNetwork(), "ignite")
 	return m, nil
 }
 
-func (p) Execute(c plugin.ExecutedCommand) error {
+func (p) Execute(_ context.Context, c *plugin.ExecutedCommand) error {
 	// Instead of a switch on c.Use, we run the root command like if
 	// we were in a command line context. This implies to set os.Args
 	// correctly.
-	// Remove the first arg "ignite" from OSArgs because our network
+	// Remove the first arg "ignite" from OsArgs because our network
 	// command root is "network" not "ignite".
-	os.Args = c.OSArgs[1:]
+	os.Args = c.OsArgs[1:]
 	return cmd.NewNetwork().Execute()
 }
 
-func (p) ExecuteHookPre(hook plugin.ExecutedHook) error {
-	return nil
-}
-
-func (p) ExecuteHookPost(hook plugin.ExecutedHook) error {
-	return nil
-}
-
-func (p) ExecuteHookCleanUp(hook plugin.ExecutedHook) error {
-	return nil
-}
-
 func main() {
-	pluginMap := map[string]hplugin.Plugin{
-		"cli-plugin-network": &plugin.InterfacePlugin{Impl: &p{}},
-	}
-
 	hplugin.Serve(&hplugin.ServeConfig{
 		HandshakeConfig: plugin.HandshakeConfig(),
-		Plugins:         pluginMap,
+		Plugins: map[string]hplugin.Plugin{
+			"cli-plugin-network": plugin.NewGRPC(&p{}),
+		},
+		GRPCServer: hplugin.DefaultGRPCServer,
 	})
 }
