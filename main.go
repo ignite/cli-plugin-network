02217635c4a94d14d32b3f2fb25d6142a0dc08bd
Jerónimo Albi <jeronimo.albi@gmail.com>
2023-11-13 17:48:31 +0100

    refactor: update plugin to use gRPC (#22)
    * refactor: update plugin to use gRPC
    
    * chore: update Go and Ignite dependency version
    
    * fix: correct port data type
    
    * chore: change gitignore to exclude app binaries
    
    * refactor: change plugin to support the client API
    
    * chore: update dependencies
    
    * ci: update workflows to work with Go `v1.21`
    
    * chore: remove unused golangci-lint linter
    
    * chore: fix linting issues

--- main.go
+++ main.go
@@ -1,61 +1,51 @@
 package main
 
 import (
-	"encoding/gob"
+	"context"
 	"os"
 
 	hplugin "github.com/hashicorp/go-plugin"
-
 	"github.com/ignite/cli/ignite/services/plugin"
 
 	"github.com/ignite/cli-plugin-network/cmd"
 )
 
-func init() {
-	gob.Register(plugin.Manifest{})
-	gob.Register(plugin.ExecutedCommand{})
-	gob.Register(plugin.ExecutedHook{})
-}
+type app struct{}
 
-type p struct{}
-
-func (p) Manifest() (plugin.Manifest, error) {
-	m := plugin.Manifest{
-		Name: "network",
-	}
+func (app) Manifest(context.Context) (*plugin.Manifest, error) {
+	m := &plugin.Manifest{Name: "network"}
 	m.ImportCobraCommand(cmd.NewNetwork(), "ignite")
 	return m, nil
 }
 
-func (p) Execute(c plugin.ExecutedCommand) error {
+func (app) Execute(_ context.Context, c *plugin.ExecutedCommand, _ plugin.ClientAPI) error {
 	// Instead of a switch on c.Use, we run the root command like if
 	// we were in a command line context. This implies to set os.Args
 	// correctly.
-	// Remove the first arg "ignite" from OSArgs because our network
+	// Remove the first arg "ignite" from OsArgs because our network
 	// command root is "network" not "ignite".
-	os.Args = c.OSArgs[1:]
+	os.Args = c.OsArgs[1:]
 	return cmd.NewNetwork().Execute()
 }
 
-func (p) ExecuteHookPre(hook plugin.ExecutedHook) error {
+func (app) ExecuteHookPre(context.Context, *plugin.ExecutedHook, plugin.ClientAPI) error {
 	return nil
 }
 
-func (p) ExecuteHookPost(hook plugin.ExecutedHook) error {
+func (app) ExecuteHookPost(context.Context, *plugin.ExecutedHook, plugin.ClientAPI) error {
 	return nil
 }
 
-func (p) ExecuteHookCleanUp(hook plugin.ExecutedHook) error {
+func (app) ExecuteHookCleanUp(context.Context, *plugin.ExecutedHook, plugin.ClientAPI) error {
 	return nil
 }
 
 func main() {
-	pluginMap := map[string]hplugin.Plugin{
-		"cli-plugin-network": &plugin.InterfacePlugin{Impl: &p{}},
-	}
-
 	hplugin.Serve(&hplugin.ServeConfig{
 		HandshakeConfig: plugin.HandshakeConfig(),
-		Plugins:         pluginMap,
+		Plugins: map[string]hplugin.Plugin{
+			"cli-plugin-network": plugin.NewGRPC(&app{}),
+		},
+		GRPCServer: hplugin.DefaultGRPCServer,
 	})
 }
